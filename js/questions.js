e02cad07cf0b94def16218759a7dd43e52bdb4db
Zero_X <rvkov536@gmail.com>
2023-06-13 23:24:46 -0400

    quiz app is 90 percent complete, need to add functionality and to set the item so that player data is saved to local storage, a couple of minor styling bugs need polishing as well

--- js/questions.js
+++ js/questions.js
@@ -1,60 +1,60 @@
 const questionData = [
     {
-        question: "1. Which of the following is a markup language used for structuring the content on web pages?",
+        question: "Which of the following is a markup language used for structuring the content on web pages?",
         choices: ['CSS', 'JavaScript', 'HTML', 'PHP'],
         correctAnswer: "HTML"
     },
 
     {
-        question: "2. Which CSS property is used to change the text color of an element?",
+        question: "Which CSS property is used to change the text color of an element?",
         choices: ['color', 'background-color', 'font - color', 'text - color'],
         Answer: "color"
     },
 
     {
-        question: "3. Which of the following is NOT a valid CSS selector?",
+        question: "Which of the following is NOT a valid CSS selector?",
         choices: ['.class-selector', '#id - selector', '$element - selector', 'element- selector'],
         Answer: "$element - selector"
     },
 
     {
-        question: "4. Which of the following is NOT a JavaScript framework or library for front - end development?",
+        question: "Which of the following is NOT a JavaScript framework or library for front - end development?",
         choices: ['React', 'Angular', 'Bootstrap', 'jQuery'],
         Answer: "Bootstrap"
     },
 
     {
-        question: "5. What is the purpose of CSS media queries?",
+        question: "What is the purpose of CSS media queries?",
         choices: ['To include external CSS filesb', 'To apply different styles based on the device screen size', 'To add interactivity to web pages', 'To manipulate the DOM'],
         Answer: "To apply different styles based on the device screen size"
     },
 
     {
-        question: "6. Which HTML tag is used to link an external CSS file to an HTML document?",
+        question: "Which HTML tag is used to link an external CSS file to an HTML document?",
         choices: ['<link>', '<style>', '<script>', '<css>'],
         Answer: "<link>"
     },
 
     {
-        question: "7. Which CSS property is used to control the spacing between the content and border of an element?",
+        question: "Which CSS property is used to control the spacing between the content and border of an element?",
         choices: ['margin', 'padding', 'border', 'spacing'],
         Answer: "padding"
     },
 
     {
-        question: "8. What does the acronym 'SEO' stand for in web development?",
+        question: "What does the acronym 'SEO' stand for in web development?",
         choices: ['Search Engine Optimization', 'Secure External Object', 'Single-Page Application', 'Style Encapsulation Object'],
         Answer: "Search Engine Optimization"
     },
 
     {
-        question: "9. Which of the following is NOT a valid CSS display property value?",
+        question: "Which of the following is NOT a valid CSS display property value?",
         choices: ['block', 'inline', 'visible', 'flex'],
         Answer: "visible"
     },
     
     {
-        question: "10. Which CSS unit of measurement is relative to the font size of the element?",
+        question: "Which CSS unit of measurement is relative to the font size of the element?",
         choices: ['px', 'em', '% (percentage)', 'rem'],
         Answer: "em"
     }
--- js/script.js
+++ js/script.js
@@ -5,9 +5,10 @@ const leaderboard = document.getElementById('leaderboard');
 const quizContainer = document.getElementById('quiz-container');
 const questions = document.getElementById('question');
 const choices = document.getElementById('choices');
+const timerTitle = document.getElementById('timer-title');
 
 const countdownElement = document.getElementById('countdown');
-var quizTimer= document.getElementById('quiz-timer');
+var quizTimer = document.getElementById('quiz-timer');
 var timer;
 var penalty = 6;
 
@@ -25,22 +26,38 @@ function startQuiz() {
   quizContainer.classList.remove('hide')
   quizTimer.style.display = ('hide');
   startTimer()
+  showQuestion(0);
 }
 // function that sets the questions from the questions.js file, in order, starting with question 1, ending with question 10
 
 function startTimer() {
   var time = 60;
 
-  timer = setInterval(function (startTimer) {
+  timer = setInterval(function () {
     time--;
-    countdownElement.textContent = time;
+    countdownElement.textContent = time.toString();
 
     if (time <= 0) {
       clearInterval(timer);
-      // Time's up! Handle the event here.
       console.log("Game Over!");
     }
   }, 1000);
+}
+
+function showQuestion(index) {
+  const question = questionData[index];
+  questions.textContent = `Question ${index + 1}: ${question.question}`;
+
+  choices.innerHTML = '';
+
+  for (let j = 0; j < question.choices.length; j++) {
+    const choice = document.createElement('button');
+    choice.textContent = question.choices[j];
+    choice.addEventListener('click', function () {
+      checkAnswer(choice, question.correctAnswer);
+    });
+    choices.appendChild(choice);
+  }
 }
 
 function questionSequence(data) {
@@ -55,9 +72,84 @@ function questionSequence(data) {
     for (let j = 0; j < answerChoices.length; j++) {
       const choice = document.createElement('button');
       choice.textContent = answerChoices[j];
+      choice.addEventListener('click', function () {
+        checkAnswer(choice, data[i].correctAnswer);
+      });
       choices.appendChild(choice);
     }
   }
 }
+
+// function to check the selected answer
+function checkAnswer(selectedChoice, correctAnswer) {
+  if (selectedChoice.textContent === correctAnswer) {
+    selectedChoice.classList.add('correct');
+    score++;
+  } else {
+    selectedChoice.classList.add('incorrect');
+    penalize();
+  }
+
+  disableChoices();
+  showNextButton();
+}
+
+// function to penalize for incorrect answers
+function penalize() {
+  countdownElement.textContent = parseInt(countdownElement.textContent) - penalty;
+}
+
+// function to disable the answer choices
+function disableChoices() {
+  const answerButtons = document.querySelectorAll('#choices button');
+  answerButtons.forEach((button) => {
+    button.disabled = true;
+  });
+}
+
+// function to show the next question button
+function showNextButton() {
+  continueButton.classList.remove('hide');
+  disableChoices();
+}
+
+// function to handle the next question button click
+continueButton.addEventListener('click', function () {
+  continueButton.classList.add('hide');
+  currentQuestionIndex++;
+
+  if (currentQuestionIndex < questionData.length) {
+    showQuestion(currentQuestionIndex);
+  } else {
+    endQuiz();
+  }
+});
+
+// function to end the quiz and display the leaderboard
+function endQuiz() {
+  clearInterval(timer);
+  countdownElement.textContent = 'Game Over!';
+  choices.innerHTML = '';
+  quizContainer.classList.add('hide');
+  leaderboard.classList.remove('hide');
+  timerTitle.classList.add('hide');
+
+  const gameOverParagraph = document.getElementById('game-over');
+  gameOverParagraph.textContent = 'Game Over!';
+  gameOverParagraph.style.fontSize = '100px';
+  gameOverParagraph.style.color = '#ffd23f';
+  gameOverParagraph.style.textShadow = '4px 4px #ee4266';
+  gameOverParagraph.style.marginTop = '100px';
+  gameOverParagraph.style.marginBottom = 'auto';
+  gameOverParagraph.style.textAlign = 'center';
+
+  // leaderboard.style.marginTop = '100px';
+
+  // TODO: Implement logic to display the leaderboard with scores
+
+  console.log('Quiz ended');
+}
+
+// TODO: Implement logic to display the leaderboard with scores
 
 questionSequence(questionData);
